632aa00a641cace9f603474a60f1e29f09a206d1
Amulyam24 <amulmek1@in.ibm.com>
2022-12-02 16:35:50 +0530

    Encryption

--- caa-image.sh
+++ caa-image.sh
@@ -3,11 +3,15 @@ set -o errexit
 set -o nounset
 set -o pipefail
  
-KATA_AGENT_SRC="/root/go/src/github.com/kata-containers/src/agent"
-CLOUD_API_ADAPTER="/root/go/src/github.com/cloud-api-adaptor"
-SKOPEO="/root/go/src/github.com/skopeo"
-UMOCI="/root/go/src/github.com/umoci"
+REPO_ROOT="/root/go/src/github.com"
+KATA_AGENT_SRC="${REPO_ROOT}/kata-containers/src/agent"
+CLOUD_API_ADAPTER="${REPO_ROOT}/cloud-api-adaptor"
+SKOPEO="${REPO_ROOT}/skopeo"
+UMOCI="${REPO_ROOT}/umoci"
+AA_SRC="${REPO_ROOT}/attestation-agent"
 KATA_AGENT_BIN="target/powerpc64le-unknown-linux-gnu/release/kata-agent"
+AA_BIN="target/powerpc64le-unknown-linux-gnu/release/attestation-agent"
+BIN_DIR="/usr/local/bin"
 
 #PAUSE=$(FILES_DIR)/$(PAUSE_BUNDLE)/rootfs/pause
 PAUSE_SRC="pause"
@@ -15,7 +19,6 @@ PAUSE_REPO="docker://k8s.gcr.io/pause"
 PAUSE_VERSION="3.6"
 PAUSE_BUNDLE="pause_bundle"
 
-REPO_ROOT="/root/go/src/github.com"
 mkdir -p ${REPO_ROOT}
 
 #Install golangi and rust
@@ -26,30 +29,35 @@ export GOPATH="/root/go"
 
 curl --proto '=https' --tlsv1.2 -sSf -o /tmp/rustup-init https://sh.rustup.rs
 sh /tmp/rustup-init -y
-rm /tmp/rustup-init
+rm -f /tmp/rustup-init
 source /root/.cargo/env
 rustup target add powerpc64le-unknown-linux-gnu 
 
 #Install dependenices
-yum install -y protobuf-compiler libseccomp-devel
+yum install -y protobuf-compiler libseccomp-devel openssl openssl-devel
 
 cd ${REPO_ROOT}
 # clone the repos
 git clone -b staging https://github.com/confidential-containers/cloud-api-adaptor.git
-git clone -b CCv0 https://github.com/kata-containers/kata-containers.git
+git clone -b CCv0 https://github.com/Amulyam24/kata-containers.git
 git clone -b v0.4.7 https://github.com/opencontainers/umoci
+git clone https://github.com/confidential-containers/attestation-agent.git
 
 # Install binaries
 cd ${CLOUD_API_ADAPTER} && CLOUD_PROVIDER=ibmcloud make agent-protocol-forwarder
-install agent-protocol-forwarder /usr/local/bin/agent-protocol-forwarder
+install agent-protocol-forwarder ${BIN_DIR}
 
 yum install -y skopeo-1.5.0
 
 cd ${UMOCI} && make
-install umoci /usr/local/bin/umoci
+install umoci ${BIN_DIR}
 
 cd ${KATA_AGENT_SRC} && make BUILD_TYPE=release
-install ${KATA_AGENT_BIN} /usr/local/bin/kata-agent
+install ${KATA_AGENT_BIN} ${BIN_DIR}
+
+cd ${AA_SRC}/app
+cargo build --release --no-default-features --features offline_fs_kbc --target powerpc64le-unknown-linux-gnu
+install ${AA_BIN} ${BIN_DIR}
 
 cd /utilities
 #Embed the pause image
@@ -58,6 +66,7 @@ umoci unpack --image ${PAUSE_SRC}:${PAUSE_VERSION} files/${PAUSE_BUNDLE}
 
 # Copy files
 sudo mkdir -p /etc/kata-containers
+sudo cp -a files/etc/agent-config.toml /etc/agent-config.toml
 sudo cp -a files/etc/containers/* /etc/containers/
 sudo cp -a files/etc/systemd/* /etc/systemd/
 sudo cp -a files/pause_bundle /
@@ -67,3 +76,5 @@ systemctl enable agent-protocol-forwarder
 systemctl enable kata-agent
 systemctl enable run-kata\\x2dcontainers.mount 
 systemctl enable run-kata\\x2dcontainers-shared-containers.mount
+
+yum remove -y skoepo
