44bb0cdcdf574fcff01ad8f738d60599992621aa
Amulyam24 <amulmek1@in.ibm.com>
2023-03-21 14:59:25 +0530

    normal image

--- caa-image.sh
+++ caa-image.sh
@@ -6,11 +6,9 @@ set -o pipefail
 REPO_ROOT="/root/go/src/github.com"
 KATA_AGENT_SRC="${REPO_ROOT}/kata-containers/src/agent"
 CLOUD_API_ADAPTER="${REPO_ROOT}/cloud-api-adaptor"
-SKOPEO="${REPO_ROOT}/skopeo"
 UMOCI="${REPO_ROOT}/umoci"
-AA_SRC="${REPO_ROOT}/attestation-agent"
+
 KATA_AGENT_BIN="target/powerpc64le-unknown-linux-gnu/release/kata-agent"
-AA_BIN="target/powerpc64le-unknown-linux-gnu/release/attestation-agent"
 BIN_DIR="/usr/local/bin"
 
 #PAUSE=$(FILES_DIR)/$(PAUSE_BUNDLE)/rootfs/pause
@@ -22,8 +20,8 @@ PAUSE_BUNDLE="pause_bundle"
 mkdir -p ${REPO_ROOT}
 
 #Install golangi and rust
-wget https://go.dev/dl/go1.18.6.linux-ppc64le.tar.gz
-sudo tar -C /usr/local -xf go1.18.6.linux-ppc64le.tar.gz 
+wget https://go.dev/dl/go1.19.6.linux-ppc64le.tar.gz
+sudo tar -C /usr/local -xf go1.19.6.linux-ppc64le.tar.gz 
 export PATH=$PATH:/usr/local/go/bin:/usr/local/bin/
 export GOPATH="/root/go"
 
@@ -41,7 +39,6 @@ cd ${REPO_ROOT}
 git clone -b staging https://github.com/confidential-containers/cloud-api-adaptor.git
 git clone -b CCv0 https://github.com/Amulyam24/kata-containers.git
 git clone -b v0.4.7 https://github.com/opencontainers/umoci
-git clone https://github.com/confidential-containers/attestation-agent.git
 
 # Install binaries
 cd ${CLOUD_API_ADAPTER} && CLOUD_PROVIDER=ibmcloud make agent-protocol-forwarder
@@ -55,11 +52,6 @@ install umoci ${BIN_DIR}
 cd ${KATA_AGENT_SRC} && make BUILD_TYPE=release
 install ${KATA_AGENT_BIN} ${BIN_DIR}
 
-cd ${AA_SRC}/app
-OPENSSL=1
-cargo build --release --no-default-features --features offline_fs_kbc,grpc,openssl --target powerpc64le-unknown-linux-gnu
-install ${AA_BIN} ${BIN_DIR}
-
 cd /utilities
 #Embed the pause image
 skopeo --policy files/etc/containers/policy.json copy ${PAUSE_REPO}:${PAUSE_VERSION} oci:${PAUSE_SRC}:${PAUSE_VERSION}
@@ -67,9 +59,6 @@ umoci unpack --image ${PAUSE_SRC}:${PAUSE_VERSION} files/${PAUSE_BUNDLE}
 
 # Copy files
 sudo mkdir -p /etc/kata-containers
-sudo cp -a files/etc/agent-config.toml /etc/agent-config.toml
-sudo cp -a files/etc/aa-offline_fs_kbc-keys.json /etc/aa-offline_fs_kbc-keys.json
-sudo cp -a files/etc/aa-offline_fs_kbc-resources.json /etc/aa-offline_fs_kbc-resources.json
 sudo cp -a files/etc/containers/* /etc/containers/
 sudo cp -a files/etc/systemd/* /etc/systemd/
 sudo cp -a files/pause_bundle /
